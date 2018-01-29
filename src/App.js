52b200e75d5c86b4e039825b2da45e642065e285
Sandhya Mahat <33580255+Sannu8@users.noreply.github.com>
2018-01-29 12:13:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Route, Link } from 'react-router-dom';
 
+import Header from './Header';
+import Button from './Button';
 import Info from './Info.js';
 import Skills from './Skills.js';
 import Portfolio from './Portfolio.js';
@@ -8,12 +11,33 @@ import Portfolio from './Portfolio.js';
 class App extends Component {
   render() {
     return (
+    <div>
+    
+    <BrowserRouter>
+      <div>
+      <Route path="/" component= {Header} />
+      <Route path="/personal" component= {Info} />
+      <Route path="/skills" component= {Skills} />
+      <Route path="/portfolio" component= {Portfolio} />
+      </div>
+    </BrowserRouter>
 
-    <form class="" action="index.html" method="post">
+    <table>
+      <tbody>
+        <tr>
+        <Button title="Personal" />
+        <Button title="Skills" />
+        <Button title="Portfolio" />
+        </tr>
+      </tbody>
+    </table>
+
+
     <Info />
     <Skills />
     <Portfolio />
-    </form>
+
+    </div>
     );
   }
 }
