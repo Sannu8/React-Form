05d4e707f2add290a67453d5a8f2489783cd4937
Sannu <smahat.official@gmail.com>
2018-06-13 00:24:04 +0300

    Info test file added

--- src/Info.test.js
+++ src/Info.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import Info from './Info';
 
 import { shallow } from 'enzyme';
+
+it('renders the heading of component info', () => {
+    const wrapper = shallow(<Info />);
+    expect(wrapper.find('h3').text()).toMatch(/info/);
+
+})
